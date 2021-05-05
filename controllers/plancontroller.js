c23750ef42c5b390521851ee45bdef8f8f4358d6
Bob Roudebush <bob@roudybob.net>
2021-05-05 13:59:11 -0400

    fixes to delete plan endpoint

--- controllers/plancontroller.js
+++ controllers/plancontroller.js
@@ -151,43 +151,41 @@ router.get('/:id', validateSession, function (req, res) {
 
 // Only should allow deletions if the plan belongs to the current user OR that user's coach
 router.delete('/:id', validateSession, function (req, res) {
-    
-    // Get the info about the plan to delete
-    Plan.findOne({
-        where: {
-            id: req.params.id
-        },
-    })
-    .then((plan) => {
-        console.log(req.user);
-        if (req.user.team) {
-            if (!req.user.team.runners.includes(plan.userId)) {
-                // Deny access if not a coach and the id doesn't match one of their runners
-                return res.status(403).json({ message: "You are not this runner's coach." })
-            } else {
-                // This person is the runner's coach
-                const query = {
-                    where: {id: req.params.id} // delete the plan even if it's not owned by current user
-                }
+    if (!req.user.coach) {
+        const query = {
+            where: {id: req.params.id, userId: req.user.id } // Only find the plan if the userid matches.
+        }
 
+        Plan.destroy(query)
+        .then((rowsAffected) => res.status(200).json({message: `${rowsAffected} entries deleted.`}))
+        .catch((err) => res.status(500).json({ error: err }));
+
+    } else {
+        const query = {
+            where: {id: req.params.id} 
+        }
+
+        Plan.findOne(query)
+        .then((plan) => {
+            if (req.user.id == plan.userId) {
+                // User is a coach but this is their plan
                 Plan.destroy(query)
                 .then((rowsAffected) => res.status(200).json({message: `${rowsAffected} entries deleted.`}))
                 .catch((err) => res.status(500).json({ error: err }));
+            } else if (req.user.team.runners) {
+                if (req.user.team.runners.includes(parseInt(plan.userId))) {
+                    Plan.destroy(query)
+                    .then((rowsAffected) => res.status(200).json({message: `${rowsAffected} entries deleted.`}))
+                    .catch((err) => res.status(500).json({ error: err }));
+                } else {
+                    res.status(403).json({ message: "Unauthorized - You are not this runner's coach." })
+                }
+            } else if (req.user.team.runners == null) {
+                res.status(500).json({ message: "(NULL) Runners Array is Empty" })
             }
-        } else {
-            // This person is the runner
-            const query = {
-                where: {id: req.params.id, userId: req.user.id}
-            }
-
-            Plan.destroy(query)
-            .then((rowsAffected) => res.status(200).json({message: `${rowsAffected} entries deleted.`}))
-            .catch((err) => res.status(500).json({ error: err }));
-        }
-    
-    })
-    .catch((err) => res.status(500).json({ error: err }))
-
-})
+        })
+        .catch((err) => res.status(500).json({ error: err, query: query }));
+    }
+});
 
 module.exports = router;
