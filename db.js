10ed703870f626b37072406216d667167e495f14
Bob Roudebush <bob@roudybob.net>
2021-07-06 14:13:24 -0400

    v1 final

--- db.js
+++ db.js
@@ -5,10 +5,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
         useUTC: false, //for reading from database
-        // ssl: {
-        //     require: true,
-        //     rejectUnauthorized: false,
-        // },
+        ssl: {
+            require: true,
+            rejectUnauthorized: false,
+        },
     },
 });
 
